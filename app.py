92cb82622634e508c4e96ee9dfd0cb335481ad2e
Camilo Andrés Guevara Triana <c.guevarat@uniandes.edu.co>
2023-05-07 00:42:29 -0500

    cambios

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import subprocess
 import time
 
-subprocess.Popen(['python', './API_REST/app.py'])
-subprocess.Popen(['python', './WORKER/app.py'])
+subprocess.Popen(['python3', './API_REST/app.py'])
+subprocess.Popen(['python3', './WORKER/app.py'])
