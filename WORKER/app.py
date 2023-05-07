03840c09d90c39c8ab4ba424dc780ac2f5e001ac
nathanbelt23UniAndes <111519973+nathanbelt23UniAndes@users.noreply.github.com>
2023-05-06 20:23:01 -0500

    creacion del modelo del worker

--- WORKER/app.py
+++ WORKER/app.py
@@ -1,1 +1,19 @@
-print("Hello World")
+from flask import Flask
+from models import Base, engine
+from Listener.ListenerServicioConsulta import ListenerServicioConsulta
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_POOL_SIZE'] = 1002
+app.config['SQLALCHEMY_POOL_TIMEOUT'] = 1002
+
+
+Base.metadata.create_all(engine)
+
+@app.teardown_request
+def teardown_request(exception=None):
+   engine.dispose() # cerrar todas las conexiones de la piscina de SQLAlchemy
+
+listenerServicioConsulta=  ListenerServicioConsulta()
+listenerServicioConsulta.listenerConsultaVentas()
+
+
