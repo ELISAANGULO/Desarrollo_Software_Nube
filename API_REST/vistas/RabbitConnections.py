e8221c05622700abc06a94ae5696d48cfc862425
Camilo Andrés Guevara Triana <c.guevarat@uniandes.edu.co>
2023-05-14 19:01:19 -0500

    pub/sub qeue

--- API_REST/vistas/RabbitConnections.py
+++ API_REST/vistas/RabbitConnections.py
@@ -1,40 +1,50 @@
-from google.cloud import pubsub_v1
-from google.oauth2 import service_account
-from google.oauth2 import service_account
-from googleapiclient.discovery import build
+import pika
+import aio_pika
 class RabbitConnection():
     def __init__(self):
+        self.rabbitmq_host = '34.138.120.16'
+        self.rabbitmq_port = 5672
+        self.rabbitmq_username = 'admin'
+        self.rabbitmq_password = 'admin'
+        self.connection = None
         self.callback=None
-        self.publisher= None
-        self.topic_id = "DocumentToConvert"
-        self.project_id=""
     def crearConexion(self):
-        print("xxxxxxxxxxxxxxx  crearConexion xxxxxxxxxx")
-        credentials = service_account.Credentials.from_service_account_file(
-            'credsub.json')
-        service = build('gmail', 'v1', credentials=credentials)
-        self.project_id = "amazing-limiter-382016"
-        self.topic_id = "DocumentToConvert"
-        self.publisher = pubsub_v1.PublisherClient()
-        self.topic_path = self.publisher.topic_path(
-            self.project_id, self.topic_id)
-
+        parameters = pika.ConnectionParameters(
+            self.rabbitmq_host, self.rabbitmq_port, '/', pika.PlainCredentials(username=self.rabbitmq_username, password=self.rabbitmq_password),heartbeat=0)
+        self.connection = pika.BlockingConnection(parameters)
+        return self.connection
         
     def creacionCola(self, cola):
-        print("No necesario trabaja con GPC" + cola)
+        self.channel = self.connection.channel()
+        self.channel.queue_declare(queue=cola)
+        return self.channel
     
-    def enviarMensaje(self, mensaje,cola):
-
-        print("xxxxxxxxxxxxxxx  enviarMensaje xxxxxxxxxx")      
-        print(mensaje)
-
-        self.crearConexion()
-   
-        data = str(mensaje).encode("utf-8")
-        # When you publish a message, the client returns a future.
-        future = self.publisher.publish(self.topic_path, data)
-        print(future.result())
+    def enviarMensaje(self, cola, mensaje):
+        self.crearConexion
+        self.creacionCola(cola)
+        self.channel.basic_publish(
+            exchange='', routing_key=cola, body=f'{mensaje}')
+        print(f'Conversion enviada   {mensaje}')
+        self.cerrarConexion()
         
+    def cerrarConexion(self):
+        self.channel.stop_consuming()
+        self.connection.close()
 
+    def crearListener(self, cola):
+        #self.channel.basic_qos(prefetch_count=1)
+        self.channel.basic_consume(
+            queue=cola, on_message_callback=self.callback, auto_ack=False)
+        self.channel.start_consuming()
+    
+    def publishMessage(self, id, cola):
+        try:
+            self.crearConexion()
+            channel = self.connection.channel()
+            channel.queue_declare(queue= "DocumentToConvert")
+            channel.basic_publish(exchange='', routing_key='DocumentToConvert', body=f"{id}")
+            self.connection.close()
+        except Exception as e:
+            print("Se presentó el siguiente error", e)
 
     
--- WORKER/Listener/ListenerServicioConsulta.py
+++ WORKER/Listener/ListenerServicioConsulta.py
@@ -1,9 +1,4 @@
-from concurrent.futures import TimeoutError
-from google.cloud import pubsub_v1
-from googleapiclient.discovery import build
-from google.cloud import pubsub_v1
-from google.oauth2 import service_account
-from googleapiclient.discovery import build
+import pika
 import json
 import base64
 from models import Base, engine
@@ -26,82 +21,78 @@ class ListenerServicioConsulta():
       print("Constructor")
 
     def listenerConsultaVentas(self):
-        print("listenerConsultaVentas")
-        credentials = service_account.Credentials.from_service_account_file(
-            'credSub.json')
-        service = build('gmail', 'v1', credentials=credentials)
-        project_id = "amazing-limiter-382016"
-        topic_id = "DocumentToConvert"
-        subscription_id = "DocumentToConvert-sub"
-        # Number of seconds the subscriber should listen for messages
-        timeout = 35000
-        subscriber = pubsub_v1.SubscriberClient()
-        # The `subscription_path` method creates a fully qualified identifier
-        # in the form `projects/{project_id}/subscriptions/{subscription_id}`
-        subscription_path = subscriber.subscription_path(project_id, subscription_id)
+      print("listenerConsultaVentas")
+      parameters = pika.ConnectionParameters(
+          '34.138.120.16', 5672, '/', pika.PlainCredentials(username='admin', password='admin'), heartbeat=0)
+      connection = pika.BlockingConnection(parameters)
+      
+      channel = connection.channel()
 
-        def callback(body: pubsub_v1.subscriber.message.Message) -> None:
-                try:
-                    jsonString=str(body.data).replace('b"{', '{')
-                    jsonString = jsonString.replace('}"','}')
-                    jsonString = jsonString.replace("'", '"')
-                    aDict = json.loads(jsonString)
-                    #engine = create_engine('postgresql://postgres:postgres@localhost:5432/conversor')
-                    db_uri = 'postgresql://postgres:andes@35.196.5.117:5432/conversor'
-                    engine = create_engine(db_uri)
+      #channel.exchange_declare(exchange='', exchange_type='fanout')
 
-                    Session = sessionmaker(bind=engine)
-                    session = Session()
-                    nombre_archivo_original_gcp = aDict["nombre_archivo_original_gcp"]
-                    nombre_archivo_convertido_gcp = aDict["nombre_archivo_convertido_gcp"]
-                    
-                    conversion = session.query(Conversion).filter(
-                        Conversion.nombre_archivo_convertido_gcp == nombre_archivo_convertido_gcp ).first()
-                    
-                    base64_archivo_original = conversion.archivo_base64_original
-                    archivo_bytes = base64.b64decode(base64_archivo_original)
-                    formato_destino = conversion.extension_destino
+      result = channel.queue_declare(queue='DocumentToConvert', exclusive=False)
+      queue_name = result.method.queue
 
-                    with open(nombre_archivo_original_gcp, 'wb') as f:
-                        f.write(np.array(archivo_bytes))
+      #channel.queue_bind(exchange='logs', queue=queue_name)
 
-                    archivo = Archivo(nombre_archivo_original_gcp)
-                    nuevoArchivo=""
-                    if (formato_destino == "pdf"):  
-                        nuevoArchivo=archivo.comprimir_a_tar_gz(nombre_archivo_original_gcp,nombre_archivo_convertido_gcp)
-                    elif (formato_destino == "tar.gz"):
-                        nuevoArchivo=archivo.comprimir_a_tar_gz(nombre_archivo_original_gcp,nombre_archivo_convertido_gcp)
-                    elif (formato_destino == "zip"):
-                        nuevoArchivo=archivo.comprimir_a_zip(nombre_archivo_original_gcp,nombre_archivo_convertido_gcp)
-                    elif (formato_destino == "7z"):
-                        nuevoArchivo=archivo.comprimir_a_7z(nombre_archivo_original_gcp,nombre_archivo_convertido_gcp)
-                    
-                    
-                    GcpCloudStorage.upload_to_bucket(
-                        nombre_archivo_convertido_gcp, nombre_archivo_convertido_gcp, "Convertidos/")
-                    GcpCloudStorage.upload_to_bucket(
-                        nombre_archivo_original_gcp, nombre_archivo_original_gcp, "Originales/")
-                    conversion.disponible = True
-                    #conversion.archivo__base64_convertido = nuevoArchivo
-                    conversion.status= 'processed'
-                    session.commit()
-                    session.close()
-                    os.remove(nombre_archivo_convertido_gcp)
-                    os.remove(nombre_archivo_original_gcp)
-                
-                except Exception as e:
-                    print("Se presentó el siguiente error", e)
-                body.ack()
-        streaming_pull_future=subscriber.subscribe(
-        subscription_path, callback = callback)
-        print(f"Listening for messages on {subscription_path}..\n")
-            # Wrap subscriber in a 'with' block to automatically call close() when done.
-        with subscriber:
-            try:
-                 # When `timeout` is not set, result() will block indefinitely,
-                # unless an exception is encountered first.
-                streaming_pull_future.result(timeout = timeout)
-            except TimeoutError:
-                streaming_pull_future.cancel()  # Trigger the shutdown.
-                # Block until the shutdown is complete.
-                streaming_pull_future.result()
+   
+
+
+      def callback(ch, method, properties, body):
+          try:
+              jsonString = str(body).replace('b"{','{')
+              jsonString = jsonString.replace('}"','}')
+              jsonString = jsonString.replace("'", '"')
+              aDict = json.loads(jsonString)
+              #engine = create_engine('postgresql://postgres:postgres@localhost:5432/conversor')
+              db_uri = 'postgresql://postgres:andes@35.196.5.117:5432/conversor'
+              engine = create_engine(db_uri)
+
+              Session = sessionmaker(bind=engine)
+              session = Session()
+              nombre_archivo_original_gcp = aDict["nombre_archivo_original_gcp"]
+              nombre_archivo_convertido_gcp = aDict["nombre_archivo_convertido_gcp"]
+              
+            #   conversion = session.query(Conversion).filter(
+            #       Conversion.nombre_archivo_convertido_gcp == nombre_archivo_convertido_gcp ).first()
+              print("###########"+nombre_archivo_convertido_gcp)
+              conversion = session.query(Conversion).filter(Conversion.nombre_archivo_convertido_gcp == nombre_archivo_convertido_gcp).first()
+              #conversion = Conversion.query.filter_by(nombre_archivo_convertido_gcp=nombre_archivo_convertido_gcp).first()
+              base64_archivo_original = conversion.archivo_base64_original
+              archivo_bytes = base64.b64decode(base64_archivo_original)
+              formato_destino = conversion.extension_destino
+
+              with open(nombre_archivo_original_gcp, 'wb') as f:
+                f.write(np.array(archivo_bytes))
+
+              archivo = Archivo(nombre_archivo_original_gcp)
+              nuevoArchivo=""
+              if (formato_destino == "pdf"):  
+                    nuevoArchivo=archivo.comprimir_a_tar_gz(nombre_archivo_original_gcp,nombre_archivo_convertido_gcp)
+              elif (formato_destino == "tar.gz"):
+                    nuevoArchivo=archivo.comprimir_a_tar_gz(nombre_archivo_original_gcp,nombre_archivo_convertido_gcp)
+              elif (formato_destino == "zip"):
+                    nuevoArchivo=archivo.comprimir_a_zip(nombre_archivo_original_gcp,nombre_archivo_convertido_gcp)
+              elif (formato_destino == "7z"):
+                    nuevoArchivo=archivo.comprimir_a_7z(nombre_archivo_original_gcp,nombre_archivo_convertido_gcp)
+              
+             
+              GcpCloudStorage.upload_to_bucket(
+                  nombre_archivo_convertido_gcp, nombre_archivo_convertido_gcp, "Convertidos/")
+              GcpCloudStorage.upload_to_bucket(
+                  nombre_archivo_original_gcp, nombre_archivo_original_gcp, "Originales/")
+              conversion.disponible = True
+              #conversion.archivo__base64_convertido = nuevoArchivo
+              conversion.status= 'processed'
+              session.commit()
+              session.close()
+              os.remove(nombre_archivo_convertido_gcp)
+              os.remove(nombre_archivo_original_gcp)
+          except Exception as e:
+            print("Se presentó el siguiente error", e)
+          
+
+      channel.basic_consume(
+          queue=queue_name, on_message_callback=callback, auto_ack=True)
+
+      channel.start_consuming()
